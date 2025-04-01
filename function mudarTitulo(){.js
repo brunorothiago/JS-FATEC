667489ef86ee043ccee0bc8a950602b0d618a7d9
brunorothiago <137723082+brunorothiago@users.noreply.github.com>
2025-04-01 10:05:20 -0300

    Add files via upload

---
+++ function mudarTitulo(){.js
@@ -0,0 +1,4 @@
+function mudarTitulo(){
+    document.querySelector("h2.container__title_url-text.container_lead-package__title_url-text").innerHTML = "Europe to Trump: kiss my ass"
+}
+//a primeira parte busca a classe específica do titulo que quero mudar, e a segunda, à partir de innerHTML, faz, de fato, a alteração//
