c2fb1922282b168629e4d203ae52e324450130b1
brunorothiago <137723082+brunorothiago@users.noreply.github.com>
2025-04-01 11:36:52 -0300

    function mudarTitulo(){.js

--- function mudarTitulo(){.js
+++ function mudarTitulo(){.js
@@ -1,4 +1,6 @@
 function mudarTitulo(){
     document.querySelector("h2.container__title_url-text.container_lead-package__title_url-text").innerHTML = "Europe to Trump: kiss my ass"
+    //a primeira parte busca a classe específica do titulo que quero mudar, e a segunda, à partir de innerHTML, faz, de fato, a alteração//
 }
-//a primeira parte busca a classe específica do titulo que quero mudar, e a segunda, à partir de innerHTML, faz, de fato, a alteração//
+mudarTitulo();
+//aqui eu chamo a função//
