a1199fa452e48e3373b360ed978e5e93e4994bf8
brunorothiago <137723082+brunorothiago@users.noreply.github.com>
2025-04-03 09:34:07 -0300

    Update function mudarTitulo(){.js

--- function mudarTitulo(){.js
+++ function mudarTitulo(){.js
@@ -1,7 +1,7 @@
 //altero uma manchete do site edition.cnn.com//
 
 function mudarTitulo(){
-    document.querySelector("h2.container__title_url-text.container_lead-package__title_url-text").innerHTML = "Europe to Trump: kiss my ass"
+    document.querySelector("h1.headline_live-story__text.inline-placeholder.vossi-headline-text").innerHTML = "Trump launches historic global crybaby meltdown";
     //a primeira parte busca a classe específica do titulo que quero mudar, e a segunda, à partir de innerHTML, faz, de fato, a alteração//
 }
 mudarTitulo();
